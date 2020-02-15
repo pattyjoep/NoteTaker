734317aebaf1ea9bf71e782d354160d2021a4644
Trick <petropatrickj@gmail.com>
2020-02-15 00:15:20 -0500

    updating server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
-var fs = require("fs");
 
 
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var notes = [];
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("assets"));
 
-// Notes Data
-var notes = []
+
 
 
 // Routes 
@@ -36,17 +36,17 @@ app.get("/api/notes", function(req, res) {
 
 // Posts
 app.post("/api/notes", function(req, res) {
+  
   var newNote = req.body;
-  newNote.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  newNote.routeName = newReservation.title.replace(/\s+/g, "").toLowerCase();
     
-  if (notes.length <= 5) {
-      notes.push(newNote);
-    }
-    else {
-      reservations.push(newReservation);
-    }
-    res.json(newReservation); 
+  notes.push(newNote);
+
+  res.json(newNote); 
+
 });
+
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
